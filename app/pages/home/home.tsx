bef0cfcb90402cbcb42ed3c1d0395c6256ce5953
Josua <josua312@gmail.com>
2025-11-08 23:57:33 +0700

    Initial Commit

---
+++ app/pages/home/home.tsx
@@ -0,0 +1,10 @@
+import Button from "@mui/material/Button";
+
+export default function Home() {
+  return (
+    <>
+      <p>THIS IS HOME PAGE</p>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
