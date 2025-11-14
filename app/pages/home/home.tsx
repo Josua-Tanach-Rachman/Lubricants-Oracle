dfa44db09da2f27b4ed0613534cb288df18d2367
Josua <josua312@gmail.com>
2025-11-14 22:37:22 +0700

    add top bar

--- app/pages/home/home.tsx
+++ app/pages/home/home.tsx
@@ -1,8 +1,10 @@
 import Button from "@mui/material/Button";
+import TopBar from "~/components/topbar";
 
 export default function Home() {
   return (
     <>
+      <TopBar />
       <p>THIS IS HOME PAGE</p>
       <Button variant="contained">Hello world</Button>
     </>
