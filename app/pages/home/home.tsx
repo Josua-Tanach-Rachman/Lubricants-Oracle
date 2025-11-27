a39a1c117b295d95c6a02cc860ec4b235a850204
Yohan Leonardo <yohanleonardo150@gmail.com>
2025-11-27 13:51:57 +0700

    feat/search_popup

--- app/pages/home/home.tsx
+++ app/pages/home/home.tsx
@@ -1,10 +1,20 @@
-import Button from "@mui/material/Button";
+import Button from '@mui/material/Button'
+import { useState } from 'react'
+import SearchPopup from '../02_Search_popup/search_popup'
 
 export default function Home() {
-  return (
-    <>
-      <p>THIS IS HOME PAGE</p>
-      <Button variant="contained">Hello world</Button>
-    </>
-  );
+    const [showPopup, setShowPopup] = useState(false)
+    return (
+        <>
+            <p>THIS IS HOME PAGE</p>
+            <Button variant="contained" onClick={() => setShowPopup(true)}>
+                Hello world
+            </Button>
+
+            <SearchPopup
+                isOpen={showPopup}
+                onClose={() => setShowPopup(false)}
+            />
+        </>
+    )
 }
