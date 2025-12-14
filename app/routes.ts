091a83a4c39691b22a73a327ddac4b4953c7f32c
Michael <michaelwilliam0930@gmail.com>
2025-12-14 19:45:52 +0700

    detail page

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("pages/home/Home.tsx"),
   route("/03", "pages/03_Product_search_result/03A.tsx"),
-  route("/details/:productId", "pages/Product_Range/product_range.tsx"),
+  route("/details/:productId", "pages/detail/detail.tsx"),
   route("/test", "pages/testingProductPage.tsx"),
   route("/04/:query", "pages/04_Product_Family/04A.tsx"),
 ] satisfies RouteConfig;
